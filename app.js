f94efb2b757794f3913e25ab7acd0b8492db9f4e
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-08-24 21:49:02 +0900

    study event

--- app.js
+++ app.js
@@ -3,13 +3,26 @@ const loginForm=document.querySelector("#login-form");
 //document에서 찾는 대신에 위에 작성한 loginForm에서 검색
 const loginInput=loginForm.querySelector("input");
 //더 짧은 코드
-const loginButton=document.querySelector("#login-form button");
 
 
 //click에 대한 event 반응 함수
-function handleLoginBtnClick(){
-    console.log(username)
+//중요한건 event 가 발생할때 브라우저가 fubction울 호출한다.
+function onLoginSubmit(event){
+    //브라우저가 기본동작을 실행하지 못하도록 막는다
+    event.preventDefault();
+console.log(loginInput.value);
 }
 
-loginButton.addEventListener("click",handleLoginBtnClick)
+//form의 submit event 를 감지
+//form을 submit 할 때 입력값을 받아낸다
+
+//브라우저는 우선 onLoginSubmit function을 호출
+loginForm.addEventListener("submit",onLoginSubmit);
+
+//submit event 가 발생하면 브라우저가 알아서 저 function을 실행시켜준다.
+//()을 더하면 브라우저가 보자마자 자동으로 이 function을 실행시켜 버린다.
+// onLoginSubmit();
+
+
+
 
