80770f6a2a7be141d2896a39eab4666125a1cfd7
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-08-31 09:13:34 +0900

    study

--- array.js
+++ array.js
@@ -3,7 +3,10 @@ function sayHello(name){
 }
 
 
-Arrow Functi
+//arry Function 은 return 을 한다는게 함축되어있다.
+//괄호를 한다느건 그 안 어딘가에서 return을 하겠다는 걸 의미한다
+const sayHello = (name) => "Hello" + name;
+
 
 const lim =sayHello("Sung");
 
