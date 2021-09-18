00633446b6a293d080776c17aa3ae1699336805e
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-09-18 20:42:54 +0900

    수정

--- array.js
+++ array.js
@@ -1,8 +1,9 @@
+
 function sayHello(name){
     return "hello" + name
 }
 
-
+api.cre
 //arry Function 은 return 을 한다는게 함축되어있다.
 //괄호를 한다느건 그 안 어딘가에서 return을 하겠다는 걸 의미한다
 const sayHello = (name) => "Hello" + name;
